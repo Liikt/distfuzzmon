ef15a55ca6ea5f7a615a64d87f1b1895a0753d88
Julian <jb@futureplay.de>
2019-07-14 00:34:20 +0200

    add a comment and a new field to fuzzjob

--- client/types/types.go
+++ client/types/types.go
@@ -5,8 +5,10 @@ type ServerRespose struct {
 	Message string `json:"msg"`
 }
 
+// Fuzzjob is a struct that contains relevant data to a fuzzjob
 type Fuzzjob struct {
 	Target      string `json:"target"`
 	Fuzzer      string `json:"fuzzer"`
 	FullCommand string `json:"command"`
+	FuzzerCount int    `json:"count"`
 }
