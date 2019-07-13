ec6778055b31343f61bf78791d2700aff97fff43
Julian <jb@futureplay.de>
2019-07-13 23:36:07 +0200

    enable and disable; also streamlining the functions

--- client/client/client.go
+++ client/client/client.go
@@ -7,12 +7,11 @@ import (
 	"net/http"
 	"os"
 
-	"distfuzzmon/client/config"
 	"distfuzzmon/client/globals"
 )
 
-// Client will start the client activities
-func Client(config config.Config) {
+// RegisterClient will start the client activities
+func RegisterClient() {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:31337/api/registerclient/", globals.Conf.ServerIP), nil)
 	if err != nil {
@@ -39,8 +38,8 @@ func Client(config config.Config) {
 	}
 }
 
-// Cleanup will deregister the client when Ctrl+C was hit
-func Cleanup() {
+// DeregisterClient will deregister the client when Ctrl+C was hit
+func DeregisterClient() {
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:31337/api/deregisterclient/", globals.Conf.ServerIP), nil)
 	if err != nil {
@@ -61,6 +60,64 @@ func Cleanup() {
 	if err != nil {
 		fmt.Println("[-] Couldn't decode the response from the server.", err)
 		os.Exit(1)
+	}
+
+	if serverResp.Message != "Ok!" {
+		fmt.Println("[!] The server had some error:", serverResp.Message)
+	}
+}
+
+// EnableClient will reenable the client if it was disabled
+func EnableClient() {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:31337/api/enableclient/", globals.Conf.ServerIP), nil)
+	if err != nil {
+		fmt.Println("[-] Couldn't create a new request to the server.", err)
+		return
+	}
+
+	req.Header.Set("X-Real-IP", globals.IP)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("[-] Couldn't send the request to the server.", err)
+		return
+	}
+
+	var serverResp types.ServerRespose
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&serverResp)
+	if err != nil {
+		fmt.Println("[-] Couldn't decode the response from the server.", err)
+		return
+	}
+
+	if serverResp.Message != "Ok!" {
+		fmt.Println("[!] The server had some error:", serverResp.Message)
+	}
+}
+
+// DisableClient will reenable the client if it was disabled
+func DisableClient() {
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", fmt.Sprintf("http://%s:31337/api/disableclient/", globals.Conf.ServerIP), nil)
+	if err != nil {
+		fmt.Println("[-] Couldn't create a new request to the server.", err)
+		return
+	}
+
+	req.Header.Set("X-Real-IP", globals.IP)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println("[-] Couldn't send the request to the server.", err)
+		return
+	}
+
+	var serverResp types.ServerRespose
+	decoder := json.NewDecoder(res.Body)
+	err = decoder.Decode(&serverResp)
+	if err != nil {
+		fmt.Println("[-] Couldn't decode the response from the server.", err)
+		return
 	}
 
 	if serverResp.Message != "Ok!" {
