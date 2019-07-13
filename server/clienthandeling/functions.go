bf6f2a48003855caf357bc84ff7c636c0623b5e6
Julian <jb@futureplay.de>
2019-07-13 17:17:24 +0200

    add more communication

--- server/clienthandeling/functions.go
+++ server/clienthandeling/functions.go
@@ -10,10 +10,12 @@ import (
 // RegisterClient will register a client matching the X-Real-IP header
 func RegisterClient(w http.ResponseWriter, r *http.Request) {
 	if _, ok := types.RegisteredClients[r.RemoteAddr]; ok {
-		fmt.Printf("[-] Client from %s is already registered\n", r.RemoteAddr)
+		fmt.Printf("[~] Client from %s is already registered\n", r.RemoteAddr)
+		w.Write([]byte("{\"msg\": \"Client is already registered\"}"))
 	} else {
 		types.RegisteredClients[r.RemoteAddr] = true
 		fmt.Println("[+] Registered new clients on", r.RemoteAddr)
+		w.Write([]byte("{\"msg\": \"Ok!\"}"))
 	}
 }
 
@@ -22,27 +24,31 @@ func DeregisterClient(w http.ResponseWriter, r *http.Request) {
 	if _, ok := types.RegisteredClients[r.RemoteAddr]; ok {
 		delete(types.RegisteredClients, r.RemoteAddr)
 		fmt.Println("[+] Successfully deregistered", r.RemoteAddr)
+		w.Write([]byte("{\"msg\": \"Ok!\"}"))
 	} else {
-		fmt.Printf("[-] Client from %s was never registered\n", r.RemoteAddr)
+		fmt.Printf("[~] Client from %s was never registered\n", r.RemoteAddr)
+		w.Write([]byte("{\"msg\": \"Client never registered\"}"))
 	}
 }
 
 //EnableClient will enable the client matching the X-Real-IP header
 func EnableClient(w http.ResponseWriter, r *http.Request) {
 	if data, ok := types.RegisteredClients[r.RemoteAddr]; !ok {
-		fmt.Printf("[-] Client from %s was never registered\n", r.RemoteAddr)
+		fmt.Printf("[~] Client from %s was never registered\n", r.RemoteAddr)
 	} else if data {
 		types.RegisteredClients[r.RemoteAddr] = true
 	}
 	fmt.Println("[+] Successfully enabled", r.RemoteAddr)
+	w.Write([]byte("{\"msg\": \"Ok!\"}"))
 }
 
 //DisableClient will disable the client matching the X-Real-IP header
 func DisableClient(w http.ResponseWriter, r *http.Request) {
 	if data, ok := types.RegisteredClients[r.RemoteAddr]; !ok {
-		fmt.Printf("[-] Client from %s was never registered\n", r.RemoteAddr)
+		fmt.Printf("[~] Client from %s was never registered\n", r.RemoteAddr)
 	} else if data {
 		types.RegisteredClients[r.RemoteAddr] = false
 	}
 	fmt.Println("[+] Successfully disabled", r.RemoteAddr)
+	w.Write([]byte("{\"msg\": \"Ok!\"}"))
 }
