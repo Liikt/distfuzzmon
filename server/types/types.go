6d203b5f03986148202d7dea820e8d1a09e6a781
Julian <jb@futureplay.de>
2019-07-17 01:46:06 +0200

    fuzzjob representation on the server

--- server/types/types.go
+++ server/types/types.go
@@ -11,6 +11,15 @@ type ClientResponse struct {
 	Message string `json:"msg"`
 }
 
+// Fuzzjob is a struct that contains relevant data to a fuzzjob
+type Fuzzjob struct {
+	Target      string   `json:"target"`
+	Fuzzer      string   `json:"fuzzer"`
+	FullCommand string   `json:"command"`
+	FuzzerCount int      `json:"count"`
+	Seeds       []string `json:"seeds"`
+}
+
 var (
 	// RegisteredClients is a map that holds the information of all registered clients
 	RegisteredClients clientMap
